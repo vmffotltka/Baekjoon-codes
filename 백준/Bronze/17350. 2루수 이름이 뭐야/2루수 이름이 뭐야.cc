763b10eb834527e797818ba17496602fe34f5565
유현서 <vmffotltka1@gmail.com>
2024-11-08 10:30:09 +0900

    [Bronze III] Title: 2루수 이름이 뭐야, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17350. 2루수 이름이 뭐야/2루수 이름이 뭐야.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	bool tri = false;
+	for (int i = 0; i < n; i++) {
+		string s; cin >> s;
+		if (s == "anj") {
+			cout << "뭐야;";
+			return 0;
+		}
+	}
+	cout << "뭐야?";
+}
