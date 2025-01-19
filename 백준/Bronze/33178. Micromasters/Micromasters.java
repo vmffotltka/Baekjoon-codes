ce62958b3c3a90a0f07a05f5ee38eb85a0446c51
유현서 <vmffotltka1@gmail.com>
2025-01-20 08:54:21 +0900

    [Bronze IV] Title: Micromasters, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/33178. Micromasters/Micromasters.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        bw.write(n / 10 + "");
+        bw.flush();
+        bw.close(); br.close();
+	}
+}
