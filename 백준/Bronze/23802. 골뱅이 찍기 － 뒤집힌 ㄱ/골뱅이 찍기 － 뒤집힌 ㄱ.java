c6938d894c81af946bd7359173d17c90d4408e9e
유현서 <vmffotltka1@gmail.com>
2025-02-23 08:52:10 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㄱ, Time: 136 ms, Memory: 15040 KB -BaekjoonHub

---
+++ 백준/Bronze/23802. 골뱅이 찍기 － 뒤집힌 ㄱ/골뱅이 찍기 － 뒤집힌 ㄱ.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+        	for (int j = 0; j < n * 5; j++) bw.write("@");
+        	bw.write("\n");
+        }
+        for (int i = 0; i < n * 4; i++) {
+        	for (int j = 0; j < n; j++) bw.write("@");
+        	bw.write("\n");
+        }
+        bw.flush();
+        bw.close(); br.close();
+    }
+}
