cdab8a47be01929f68dc8c992e6ae1ba20065617
유현서 <vmffotltka1@gmail.com>
2025-07-05 11:12:17 +0900

    [Bronze III] Title: 찬반투표, Time: 108 ms, Memory: 14268 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/27736. 찬반투표/찬반투표.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int giveup = 0, approve = 0, reject = 0;
+        for (int i = 0; i < n; i++) {
+        	int p = Integer.parseInt(st.nextToken());
+        	if (p == 0) giveup++;
+        	else if (p == 1) approve++;
+        	else reject++;
+        }
+        bw.write(giveup * 2 >= n ? "INVALID" : (approve > reject ? "APPROVED" : "REJECTED"));
+        bw.flush();
+    }
+}   
