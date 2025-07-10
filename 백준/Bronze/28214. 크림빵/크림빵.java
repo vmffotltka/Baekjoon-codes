caf54cb1a23ab553be250ea4a98668c662f5e448
유현서 <vmffotltka1@gmail.com>
2025-07-10 19:54:12 +0900

    [Bronze III] Title: 크림빵, Time: 112 ms, Memory: 14540 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/28214. 크림빵/크림빵.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+        int p = Integer.parseInt(input[2]);
+        
+        input = br.readLine().split(" ");
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+        	int cnt = 0;
+        	for (int j = 0; j < k; j++) {
+            	int cur = Integer.parseInt(input[i * k + j]);
+        		if (cur == 1) cnt++;
+        	}
+        	if (cnt >= p) ans++;
+        }
+        bw.write(ans + "");
+        bw.flush();
+    }
+}
