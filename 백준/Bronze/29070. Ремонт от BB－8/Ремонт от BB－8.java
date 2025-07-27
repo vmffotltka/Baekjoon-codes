dd7bc04580c601cb4d4c45e07022746397f79e70
유현서 <vmffotltka1@gmail.com>
2025-07-28 06:53:32 +0900

    [Bronze III] Title: Ремонт от BB-8, Time: 100 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/29070. Ремонт от BB－8/Ремонт от BB－8.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int h = Integer.parseInt(input[2]);
+        int w = Integer.parseInt(input[3]);
+        
+        bw.write(((h + a - 1) / a) * ((w + b - 1) / b)  + "");
+        bw.flush();
+    }
+}
