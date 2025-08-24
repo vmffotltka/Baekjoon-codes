4584694df9bb27884d778561d9eab776b388f5d0
유현서 <vmffotltka1@gmail.com>
2025-08-24 20:38:13 +0900

    [Bronze III] Title: Average scores, Time: 524 ms, Memory: 143252 KB -BaekjoonHub

---
+++ 백준/Bronze/29965. Average scores/Average scores.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        
+        long mScore = 0, jScore = 0, mCnt = 0, jCnt = 0;
+        for (int i = 0; i < n; i++) {
+        	String[] input = br.readLine().split(" ");
+        	if (input[0].equals("M")) {
+        		mScore += Long.parseLong(input[1]);
+        		mCnt++;
+        	}
+        	else {
+        		jScore += Long.parseLong(input[1]);
+        		jCnt++;
+        	}
+        }
+        
+        double mAv = (mCnt == 0 ? 0 : mScore / Double.valueOf(mCnt));
+        double jAv = (jCnt == 0 ? 0 : jScore / Double.valueOf(jCnt));
+        
+        bw.write(mAv > jAv ? "M" : (mAv < jAv ? "J" : "V"));
+        bw.flush();
+    }
+}
