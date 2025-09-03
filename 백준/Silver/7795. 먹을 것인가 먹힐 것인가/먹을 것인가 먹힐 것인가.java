3aab9efbeb5379a28ff1b3e4bb3730e92c103216
유현서 <vmffotltka1@gmail.com>
2025-09-03 09:45:00 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 452 ms, Memory: 42668 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+        	String[] input = br.readLine().split(" ");
+        	int n = Integer.parseInt(input[0]);
+        	int m = Integer.parseInt(input[1]);
+        	
+        	int[] A = new int[n];
+        	int[] B = new int[m];
+        	
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	for (int i = 0; i < n; i++) {
+        		A[i] = Integer.parseInt(st.nextToken());
+        	}
+        	
+        	st = new StringTokenizer(br.readLine());
+        	for (int i = 0; i < m; i++) {
+        		B[i] = Integer.parseInt(st.nextToken());
+        	}
+        	
+        	Arrays.sort(B);
+        	
+        	int ans = 0;
+        	for (int i = 0; i < n; i++) {
+        		int l = 0, r = m - 1, pos = -1;
+        		while (l <= r) {
+        			int mid = (l + r) / 2;
+        			if (A[i] > B[mid]) {
+        				pos = mid;
+        				l = mid + 1;
+        			}
+        			else {
+        				r = mid - 1;
+        			}
+        		}
+        		ans += pos + 1;
+        	}
+        	sb.append(ans + "\n");
+        }
+        System.out.print(sb.toString().strip());
+    }
+}
