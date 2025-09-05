61192f01d1bf6360b8ce0dd0826dca79d20411ff
유현서 <vmffotltka1@gmail.com>
2025-09-05 13:27:23 +0900

    Create KMP(getPi).cpp

---
+++ KMP(getPi).cpp
@@ -0,0 +1,11 @@
+vector<int> getPi(string p) {
+	int m = (int)p.size(), j = 0;
+	vector<int> pi(m, 0);
+	for (int i = 1; i < m; i++) {
+		while (j > 0 && p[i] != p[j])
+			j = pi[j - 1];
+		if (p[i] == p[j])
+			pi[i] = ++j;
+	}
+	return pi;
+}
