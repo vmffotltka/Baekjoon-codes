f1397000e27bc26dfabf1ec1e9de37cfd7c66ef7
유현서 <vmffotltka1@gmail.com>
2025-09-22 09:00:58 +0900

    [Bronze V] Title: 13 Floors, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/34412. 13 Floors/13 Floors.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+    
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        
+        System.out.print(n > 12 ? n + 1 : n);
+	}
+}
