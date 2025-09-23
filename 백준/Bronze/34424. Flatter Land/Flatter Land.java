d89ec638433c9fd2e56deec6446b0a508303701e
유현서 <vmffotltka1@gmail.com>
2025-09-23 09:02:50 +0900

    [Bronze V] Title: Flatter Land, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/34424. Flatter Land/Flatter Land.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int x = Integer.parseInt(br.readLine());
+        
+        System.out.print((n - 1) * x);
+	}
+}
