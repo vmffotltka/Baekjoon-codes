88978a24ec6680cb47e1bb590e34eb1f0e0b58b1
유현서 <vmffotltka1@gmail.com>
2025-10-23 09:38:16 +0900

    [Bronze III] Title: Ложки, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/30841. Ложки/Ложки.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        
+        System.out.print(a + b == 0 ? 0 : (a * b) / (a + b));
+    }
+}
