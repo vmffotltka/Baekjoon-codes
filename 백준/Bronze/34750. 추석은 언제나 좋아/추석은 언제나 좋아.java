21fcef6f33ca63502be3342158925b1b2f845312
유현서 <vmffotltka1@gmail.com>
2025-11-20 09:19:38 +0900

    [Bronze IV] Title: 추석은 언제나 좋아, Time: 124 ms, Memory: 16024 KB -BaekjoonHub

---
+++ 백준/Bronze/34750. 추석은 언제나 좋아/추석은 언제나 좋아.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine()), p = 0;
+        if (n < 100000) p = n / 20;
+        else if (n < 500000) p = n / 10;
+        else if (n < 1000000) p = n * 3 / 20;
+        else p = n / 5;
+        
+        System.out.print(p + " " + (n - p));
+    }
+}
