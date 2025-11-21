80555ba20ae4d8338c7ad0044f6025c80ad3cdcc
유현서 <vmffotltka1@gmail.com>
2025-11-21 10:48:10 +0900

    [Bronze IV] Title: An Elephant Problem, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/34795. An Elephant Problem/An Elephant Problem.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        
+        System.out.print((d + m - 1) / m);
+    }
+}
