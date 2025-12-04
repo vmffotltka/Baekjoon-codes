da191015d2c634c188d11f9d758c08b7fd220dbf
유현서 <vmffotltka1@gmail.com>
2025-12-04 09:07:40 +0900

    [Bronze III] Title: 수박 게임, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/31868. 수박 게임/수박 게임.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        while (n-- > 1) k /= 2;
+
+        System.out.print(k);
+    }
+}
