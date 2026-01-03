7ff60074ae09182919b66d2f5cd9eba6e6e7919d
유현서 <vmffotltka1@gmail.com>
2026-01-03 15:07:29 +0900

    [Bronze III] Title: 아카라카 2, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/32652. 아카라카 2/아카라카 2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder().append("AKA");
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            sb.append("RAKA");
+        }
+        System.out.print(sb);
+    }
+}
