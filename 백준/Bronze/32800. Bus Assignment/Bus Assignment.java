2bc2c6fd711d01c23cb6a5322caabed2493f8127
유현서 <vmffotltka1@gmail.com>
2026-01-13 09:18:32 +0900

    [Bronze III] Title: Bus Assignment, Time: 372 ms, Memory: 58000 KB -BaekjoonHub

---
+++ 백준/Bronze/32800. Bus Assignment/Bus Assignment.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int mx = 0, cur = 0;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            mx = Math.max(mx, cur += b - a);
+        }
+        System.out.print(mx);
+    }
+}
