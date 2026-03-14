e4f7ed3726d3a18ac32a774fc0ec118062d672fc
유현서 <vmffotltka1@gmail.com>
2026-03-14 22:57:49 +0900

    [Gold V] Title: 행복 유치원, Time: 536 ms, Memory: 53060 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        st = new StringTokenizer(br.readLine());
+        int pre = -1;
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (pre != -1)
+                pq.offer(num - pre);
+            pre = num;
+        }
+        for (int i = 0; i < k - 1; i++)
+            pq.poll();
+
+        int ans = 0;
+        while (!pq.isEmpty())
+            ans += pq.poll();
+
+        System.out.print(ans);
+    }
+}
