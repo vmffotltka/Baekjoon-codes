9fb7de9783012f2a1a2ab811bfe1e1cb4cc79102
유현서 <vmffotltka1@gmail.com>
2026-03-18 16:09:58 +0900

    [Gold IV] Title: LCS 3, Time: 168 ms, Memory: 20524 KB -BaekjoonHub

---
+++ 백준/Gold/1958. LCS 3/LCS 3.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        String s3 = br.readLine();
+
+        int[][][] dp = new int[s1.length() + 1][s2.length() + 1][s3.length() + 1];
+        int mx = 0;
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                for (int k = 1; k <= s3.length(); k++) {
+                    if (s1.charAt(i - 1) == s2.charAt(j - 1) && s2.charAt(j - 1) == s3.charAt(k - 1))
+                        dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1;
+                    else
+                        dp[i][j][k] = Math.max(Math.max(dp[i - 1][j][k], dp[i][j - 1][k]), dp[i][j][k - 1]);
+                    mx = Math.max(mx, dp[i][j][k]);
+                }
+            }
+        }
+        System.out.print(mx);
+    }
+}
