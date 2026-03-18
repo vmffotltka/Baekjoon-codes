5582c7dd0daf9537703c2b0a80db09a255409345
유현서 <vmffotltka1@gmail.com>
2026-03-18 14:09:33 +0900

    [Bronze III] Title: KUPC에 어서 오세요, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/34758. KUPC에 어서 오세요/KUPC에 어서 오세요.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(a != x && b != y ? "1\n" : "0\n");
+        }
+        System.out.print(sb);
+    }
+}
