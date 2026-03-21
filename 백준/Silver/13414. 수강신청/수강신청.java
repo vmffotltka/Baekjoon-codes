5b29adc37ce3e012f71b80ea61aa3952034af410
유현서 <vmffotltka1@gmail.com>
2026-03-21 18:43:25 +0900

    [Silver III] Title: 수강신청, Time: 584 ms, Memory: 64768 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.java
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -3,24 +3,24 @@ import java.util.*;
 
 public class Main {
 
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int k = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
-        HashMap<String, Integer> order = new HashMap<>();
+
+        LinkedHashSet<String> set = new LinkedHashSet<>();
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            order.put(s, i);
+            set.remove(s);
+            set.add(s);
         }
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(order.entrySet());
-        list.sort(Map.Entry.comparingByValue());
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < Math.min(k, list.size()); i++) {
-            sb.append(list.get(i).getKey()).append('\n');
+        Iterator<String> it = set.iterator();
+        for (int i = 0; i < k; i++) {
+            if (!it.hasNext()) break;
+            sb.append(it.next()).append('\n');
         }
         System.out.print(sb);
     }
