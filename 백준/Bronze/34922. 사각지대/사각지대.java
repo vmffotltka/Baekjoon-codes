22905f292bac0bbd14470bd6aa5ad5a249ea53b2
유현서 <vmffotltka1@gmail.com>
2026-03-24 18:42:06 +0900

    [Bronze III] Title: 사각지대, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/34922. 사각지대/사각지대.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(br.readLine());
+
+        System.out.print(w * h - (Math.PI * r * r / 4));
+    }
+}
