e49aa2f38ac4581ccc924bf7aee8648675c9b0fb
유현서 <vmffotltka1@gmail.com>
2026-03-31 23:08:42 +0900

    [Bronze III] Title: Можно и отдохнуть, Time: 324 ms, Memory: 45492 KB -BaekjoonHub

---
+++ 백준/Bronze/29145. Можно и отдохнуть/Можно и отдохнуть.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (k >= a && (k - a) % b == 0) ans += c;
+        }
+        System.out.print(ans);
+    }
+}
