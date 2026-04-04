389c265c7b1c3848e68bf0e88581883f2b221816
유현서 <vmffotltka1@gmail.com>
2026-04-04 09:46:08 +0900

    [Bronze II] Title: 이진수 덧셈, Time: 108 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.java
+++ 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
 public class Main {
 
@@ -7,25 +8,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String s1 = st.nextToken();
-        String s2 = st.nextToken();
-        StringBuilder sb = new StringBuilder();
-        int up = 0, idx1 = s1.length() - 1, idx2 = s2.length() - 1;
-        while (true) {
-            if (idx1 < 0 && idx2 < 0 && up == 0) break;
-            int digit1 = (idx1 < 0 ? 0 : s1.charAt(idx1--) - '0');
-            int digit2 = (idx2 < 0 ? 0 : s2.charAt(idx2--) - '0');
-            int sum = digit1 + digit2 + up;
-            sb.append(sum % 2);
-            up = sum / 2;
-        }
-        int idx = 0;
-        for (int i = sb.length() - 1; i >= 0; i--) {
-            if (sb.charAt(i) == '1') {
-                idx = i;
-                break;
-            }
-        }
-        System.out.print(sb.reverse().substring(sb.length() - idx - 1));
+        BigInteger b1 = new BigInteger(st.nextToken(), 2);
+        BigInteger b2 = new BigInteger(st.nextToken(), 2);
+        BigInteger sum = b1.add(b2);
+        System.out.print(sum.toString(2));
     }
 }
