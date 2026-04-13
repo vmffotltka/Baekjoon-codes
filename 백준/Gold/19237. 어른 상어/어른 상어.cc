81e3b7f410a502d127da749193cdbe2e75165283
유현서 <vmffotltka1@gmail.com>
2026-04-14 01:41:15 +0900

    [Gold I] Title: 어른 상어, Time: 0 ms, Memory: 2132 KB -BaekjoonHub

--- 백준/Gold/19237. 어른 상어/어른 상어.cc
+++ 백준/Gold/19237. 어른 상어/어른 상어.cc
@@ -1,121 +1,102 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 401;
+struct Shark {
+    int y, x, dir;
+    bool alive;
+};
 
-int dy[] = { -1, 1, 0, 0 };
-int dx[] = { 0, 0, -1, 1 };
+int dy[] = { 0, -1, 1, 0, 0 };
+int dx[] = { 0, 0, 0, -1, 1 };
+
 int N, M, K;
-int arr[20][20], t[20][20];
-int priority[MAX][4][4];
-int dir[MAX];
-queue<int> shark[20][20];
+Shark sharks[401];
+int smell_owner[20][20], smell_time[20][20];
+int priority[401][5][5];
 
-void reduceSmell() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (arr[i][j]) {
-                t[i][j]--;
-                if (!t[i][j])
-                    arr[i][j] = 0;
-            }
-        }
-    }
-}
-
-void move() {
-    vector<vector<bool>> canGo(N, vector<bool>(N));
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-			canGo[i][j] = t[i][j] == 0;
-        }
-    }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (!(!shark[i][j].empty() && t[i][j] == K)) continue;
-			int curShark = shark[i][j].front();
-            int d = dir[curShark];
-            int backY = -1, backX = -1, backDir = -1;
-            for (int k = 0; k < 4; k++) {
-				int ny = i + dy[priority[curShark][d][k]];
-				int nx = j + dx[priority[curShark][d][k]];
-				if (0 > ny || ny >= N || 0 > nx || nx >= N) continue;
-                if (arr[ny][nx] == curShark && backY == -1)
-                    backY = ny, backX = nx, backDir = k;
-                else if (canGo[ny][nx]) {
-                    arr[ny][nx] = curShark;
-					t[ny][nx] = K + 1;
-                    dir[curShark] = priority[curShark][d][k];
-					shark[ny][nx].push(curShark);
-					shark[i][j].pop();
-                    break;
-                }
-            }
-            if (!shark[i][j].empty() && shark[i][j].front() == curShark) {
-				arr[backY][backX] = curShark;
-				t[backY][backX] = K + 1;
-                dir[curShark] = priority[curShark][d][backDir];
-				shark[backY][backX].push(curShark);
-				shark[i][j].pop();
-            }
-        }
-    }
-}
-
-int removeAndRemain() {
-    int remain = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-			if (shark[i][j].empty()) continue;
-            int mn = MAX;
-            int size = shark[i][j].size();
-            for (int k = 0; k < size; k++) {
-                int curShark = shark[i][j].front();
-                mn = min(mn, curShark);
-                shark[i][j].pop();
-            }
-            arr[i][j] = mn;
-			shark[i][j].push(mn);
-            remain++;
-        }
-    }
-	return remain;
-}
-
-void input() {
-    cin >> N >> M >> K;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> arr[i][j];
-            if (arr[i][j]) {
-                shark[i][j].push(arr[i][j]);
-				t[i][j] = K;
-            }
-        }
-    }
-    for (int i = 1; i <= M; i++) {
-        cin >> dir[i];
-        dir[i]--;
-    }
-    for (int i = 1; i <= M; i++) {
-        for (int j = 0; j < 4; j++) {
-            for (int k = 0; k < 4; k++) {
-                cin >> priority[i][j][k];
-                priority[i][j][k]--;
-            }
-        }
-    }
-}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
-    input();
-    int time = 0;
-    while (++time < 1001) {
-        move();
-        reduceSmell();
-        if (removeAndRemain() == 1)
-            break;
+	cin >> N >> M >> K;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            int id; cin >> id;
+            if (id > 0) {
+				sharks[id] = { i, j, 0, true };
+				smell_owner[i][j] = id;
+				smell_time[i][j] = K;
+            }
+        }
     }
-	cout << (time == 1001 ? -1 : time);
+    for (int i = 1; i <= M; i++) cin >> sharks[i].dir;
+    for (int i = 1; i <= M; i++) {
+        for (int d = 1; d <= 4; d++) {
+            for (int k = 1; k <= 4; k++) {
+                cin >> priority[i][d][k];
+            }
+		}
+    }
+
+    int left_sharks = M, time = 0;
+    while (time < 1000) {
+        time++;
+		int next_board[20][20] = { 0 };
+        for (int i = 1; i <= M; i++) {
+            if (!sharks[i].alive) continue;
+            
+			int y = sharks[i].y, x = sharks[i].x, d = sharks[i].dir;
+			int nextY = -1, nextX = -1, nextD = -1;
+            for (int k = 1; k <= 4; k++) {
+				int nd = priority[i][d][k], ny = y + dy[nd], nx = x + dx[nd];
+                if (ny >= 0 && ny < N && nx >= 0 && nx < N) {
+                    if (smell_time[ny][nx] == 0) {
+						nextY = ny; nextX = nx; nextD = nd;
+                        break;
+                    }
+                }
+            }
+            if (nextY == -1) {
+                for (int k = 1; k <= 4; k++) {
+					int nd = priority[i][d][k], ny = y + dy[nd], nx = x + dx[nd];
+                    if (ny >= 0 && ny < N && nx >= 0 && nx < N) {
+                        if (smell_owner[ny][nx] == i) {
+                            nextY = ny; nextX = nx; nextD = nd;
+                            break;
+                        }
+                    }
+                }
+            }
+			sharks[i].y = nextY; sharks[i].x = nextX; sharks[i].dir = nextD;
+
+			if (next_board[nextY][nextX] == 0) {
+                next_board[nextY][nextX] = i;
+            }
+            else {
+                sharks[i].alive = false;
+                left_sharks--;
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (smell_time[i][j] > 0) {
+                    smell_time[i][j]--;
+                    if (smell_time[i][j] == 0) {
+                        smell_owner[i][j] = 0;
+                    }
+                }
+			}
+        }
+		for (int i = 1; i <= M; i++) {
+            if (!sharks[i].alive) continue;
+            int y = sharks[i].y, x = sharks[i].x;
+            smell_owner[y][x] = i;
+            smell_time[y][x] = K;
+        }
+        if (left_sharks == 1) {
+            cout << time;
+			return 0;
+        }
+    }
+    cout << -1;
 }
