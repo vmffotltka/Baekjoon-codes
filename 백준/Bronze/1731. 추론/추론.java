78d47a77c1d81a35c29bcff8293c3dd88cfe5c74
유현서 <vmffotltka1@gmail.com>
2026-04-26 09:31:30 +0900

    [Bronze II] Title: 추론, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/1731. 추론/추론.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+        if (arr[1] - arr[0] == arr[2] - arr[1])
+            System.out.print(arr[n - 1] + arr[1] - arr[0]);
+        else
+            System.out.print(arr[n - 1] * (arr[1] / arr[0]));
+    }
+}
